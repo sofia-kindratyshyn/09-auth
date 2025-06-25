4c015de4bb867625789757a219a98e35fe7d8256
sofia-kindratyshyn <sofia25kind@gmail.com>
2025-06-25 15:16:38 +0300

    some chngs

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,10 +6,10 @@ export default function Footer() {
       <div className={css.content}>
         <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
         <div className={css.wrap}>
-          <p>Developer: your name</p>
+          <p>Developer: S.Kindratyshyn</p>
           <p>
             Contact us:
-            <Link href='<mailto:student@notehub.app>'>student@notehub.app</Link>
+            <Link href='mailto:student@notehub.app'>student@notehub.app</Link>
           </p>
         </div>
       </div>
